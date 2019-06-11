101630e4d8fcab09a13ca05afc0c55707a101e81
wildlifehexagon <crossthebreeze@gmail.com>
2019-06-11 09:04:07 -0500

    refactor: remove console.log

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,7 @@
 import React from "react"
 
-const Home = props => {
-  console.log(process.env.REACT_APP_BASENAME)
-  return (
-    <div>
-      This is the homepage with basename {process.env.REACT_APP_BASENAME}
-    </div>
-  )
-}
+const Home = props => (
+  <div>This is the homepage with basename {process.env.REACT_APP_BASENAME}</div>
+)
 
 export default Home
